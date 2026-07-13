6fb8f22a5bb155a3023bf357c96723cc27abe2ad
ranjita <ranjita7411@gmail.com>
2026-07-13 15:17:51 +0530

    day7

---
+++ python/main.js
@@ -0,0 +1,10 @@
+function add(a,b){
+    return a+b
+}
+console.log(add(10,20))
+
+function changeText(){
+    document.getElementById("text").innerText="Text Changed";
+
+}
+let heading=document.querySelector
